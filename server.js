0c9ff52da90c5542dcf03f3a037f7d9786010daf
CardinalKing <723620021@qq.com>
2026-05-13 16:04:01 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -619,6 +619,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log('');
     console.log('==========================================');
