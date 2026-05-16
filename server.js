6dc909d331878341f5b21f0f7456076113a61b4d
CardinalKing <723620021@qq.com>
2026-05-16 18:51:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -634,13 +634,14 @@ app.get('/api/knowledge-count', (req, res) => {
 });
 
 app.use(express.static(__dirname));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
